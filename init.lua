ca9a81a524acc1424563cc1a163f106a2d71e548
intellectualPioneer <tulpejkb99@gmail.com>
2026-06-17 23:50:09 +0200

    added keymap for opening a terminal

--- init.lua
+++ init.lua
@@ -193,6 +193,10 @@ vim.diagnostic.config {
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- open terminal in split window keybind
+-- NOTE: use `<cmd>` instead of `:`
+vim.keymap.set('n', '<C-Enter>', '<cmd>split | terminal<CR>', { desc = 'Open a terminal in a split window' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
