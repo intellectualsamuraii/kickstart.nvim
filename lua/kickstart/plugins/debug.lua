7ed01353b1c3070e9d5222879662e1cd9d741cc9
intellectualPioneer <tulpejkb99@gmail.com>
2026-06-17 23:51:59 +0200

    changed keybinds for debugging

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -29,14 +29,20 @@ return {
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
-    { '<F5>', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
-    { '<F1>', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
-    { '<F2>', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
-    { '<F3>', function() require('dap').step_out() end, desc = 'Debug: Step Out' },
-    { '<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint' },
-    { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint' },
+    -- { '<F5>', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
+    { '<leader>dc', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
+    -- { '<F1>', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
+    { '<leader>ds', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
+    -- { '<F2>', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
+    { '<leader>dn', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
+    -- { '<F3>', function() require('dap').step_out() end, desc = 'Debug: Step Out' },
+    { '<leader>df', function() require('dap').step_out() end, desc = 'Debug: Step Out' },
+    { '<leader>dt', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint' },
+    { '<leader>db', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint' },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     { '<F7>', function() require('dapui').toggle() end, desc = 'Debug: See last session result.' },
+    { '<leader>dT', function() require('dap').terminate() end, desc = 'Debug: Terminate Session' },
+    -- { '<leader>dT', function() require('dapui').terminate() end, desc = 'Debug: Terminate Session' },
   },
   config = function()
     local dap = require 'dap'
